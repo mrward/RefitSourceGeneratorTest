9d4717a28622b50f8730b6070c1e9c79b4271a24
Matt Ward <matt.ward@microsoft.com>
2021-02-19 10:24:58 +0000

    Add simple console app to test Refit source generator

---
+++ RefitSourceGeneratorTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Refit;
+
+namespace RefitSourceGeneratorTest
+{
+	public interface IGitHubApi
+	{
+		[Get ("/users/{user}")]
+		Task DoSomething (string user);
+	}
+
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			try {
+				var gitHubApi = RestService.For<IGitHubApi> ("https://api.github.com");
+				Console.WriteLine ("Done");
+			} catch (Exception ex) {
+				Console.WriteLine (ex);
+			}
+		}
+	}
+}
